2297eae6905fd7bc62630b534d6dd74e773f4321
Jeroen <jegbol01@gmail.com>
2022-11-30 16:45:28 +0100

    Cleanup + header

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+
+}>()
+</script>
+
+<template>
+  <header class="bg-background py-5 px-4">
+    <nav aria-label="Top">
+      <RouterLink to="/">
+        <span class="sr-only">VRef Solutions</span>
+        <img src="@/assets/img/vref-white.png" alt="VRef Solutions"
+             class="h-11 w-auto"/>
+      </RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+        background: '#191B20'
+    }
   },
   plugins: [],
 }
